ebe351534f6847027c79a19cec296ab6d8c1f086
Sébastien Riette <sebastien.riette@meteo.fr>
2023-11-17 09:42:55 +0100

    Enable installation using ssh protocol

--- README.md
+++ README.md
@@ -3,14 +3,28 @@
 This set of tools is primarily designed to be applied on the [PHYEX](https://github.com/UMR-CNRM/PHYEX)
 repository. But they are normally generic enough to be used in other contexts.
 
-The tools can be used with the command line (the pyft.py utility can be found in the bin directory)
+The tools can be used with the command line (the pyft\_tool.py utility can be found in the bin directory)
 or as a python package (src/pyft directory). The documentation is in the doc directory and some examples
 are in the examples directory. 
 
 The fxtran binary is needed to use pyft. You can:
   - use an existing installation and make sure it can be found using your PATH variable;
-  - use an existing installation providing the exact path with the 'parser' option;
-  - install fxtran using the INSTALL.sh script (in the bin directory)
+  - use an existing installation providing the exact path with the '--parser' option;
+  - automatically install fxtran using the INSTALL.sh script (in the bin directory)
 
-To use the python package or the pfpt.py command line tool, you must source the env.sh
-file (in the bin directory). E.g. '. \<path to the current directory\>/bin/env.sh'
+To use the python package or the pfpt\_tool.py command line tool, you must source the env.sh
+file (in the bin directory). E.g. '. \<path to the current directory\>/bin/env.sh'.
+
+Prerequisites:
+  - an internet connexion (with access to the github servers) is needed only for the installation of fxtran
+  - python > 3.7 (but only tested with version 3.10)
+  - a C compiler (tested with cc 11.4.0)
+
+Quick Start Guide:
+  - open a terminal on a system satisfying the prerequisites and enter the following commands
+  - if you don't have a github ssh key or don't know what it is:
+    > git clone https://github.com/UMR-CNRM/pyft.git
+    > ./pyft/bin/INSTALL.sh
+  - if you have a github a github ssh key:
+    > git clone git@github.com:UMR-CNRM/pyft.git
+    > ./pyft/bin/INSTALL.sh --ssh
--- bin/INSTALL.sh
+++ bin/INSTALL.sh
@@ -1,33 +1,55 @@
 #!/bin/bash
 
 set -e
+set -o pipefail #abort if left command on a pipe fails
 
-#Without option, this script automatically clones and compiles the fxtran tool.
+#This script automatically clones and compiles the fxtran tool.
 #In addition a symbolic link to the fxtran binary is put in the bin directory.
+#Call the script with the -h option to get more information.
 
-#With the --clean option, the script removes the fxtran compilation.
+################################
+#### COMMAND LINE ARGUMENTS ####
+################################
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-if [ "$1" == "--clean" ]; then
-  rm -f $DIR/fxtran
-  rm -rf $DIR/../site/fxtran
-
-elif [ "$1" == "-h" ]; then
-  echo "Usage: $0 [-h] [--clean]"
+function usage {
+  echo "Usage: $0 [-h] [--clean] [--ssh]"
+  echo "--clean         remove the fxtran installation instead of installing it"
+  echo "--ssh           use the ssh protocol to clone the fxtran repository instead of https"
   echo ""
   echo "Without option, this script automatically clones and compiles the fxtran tool."
   echo "In addition a symbolic link to the fxtran binary is put in the bin directory."
   echo ""
   echo "With the --clean option, the script removes the fxtran compilation."
+}
 
-elif [ "$1" != "" ]; then
-  echo "The option '$1' does not exist for this script"
-  exit 2
+clean=0
+ssh=0
+while [ -n "$1" ]; do
+  case "$1" in
+    '-h') usage; exit;;
+    '--clean') clean=1;;
+    '--ssh') ssh=1;;
+    *) echo "The option '$1' does not exist for this script"; exit 2;;
+  esac
+  shift
+done
 
+#################################
+#### INSTALLATION / CLEANING ####
+#################################
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ $clean -eq 1 ]; then
+  rm -f $DIR/fxtran
+  rm -rf $DIR/../site/fxtran
 else
   cd $DIR/../site/
-  git clone https://github.com/pmarguinaud/fxtran
+  if [ $ssh -eq 1 ]; then
+    git clone git@github.com:pmarguinaud/fxtran.git
+  else
+    git clone https://github.com/pmarguinaud/fxtran
+  fi
   cd fxtran
   set +e
   make all
@@ -38,7 +60,7 @@ else
     echo "Don't be afraid if you see some errors; on some systems the build step is known to produce errors."
     echo "In your case everything seems to have gone smoothly, as the executable file exists."
     echo ""
-    echo "You must source the '$DIR/env.sh' file."
+    echo "To use pyft independently, you need to source the '$DIR/env.sh' file."
     cd $DIR
     ln -s $DIR/../site/fxtran/bin/fxtran .
     exit 0
